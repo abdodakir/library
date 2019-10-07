c406dc178e873d49a61b464fa2913940b12020e2
Dakir abdelati <dakir.abdelati@gmail.com>
2019-10-07 22:40:34 +0100

    update and delete user

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-"""  """
+""" This is an application """
 from flask import Flask
 from flask import jsonify, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 client = MongoClient('localhost:27017')
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app = Flask(__name__)
 db = client.library
 #export FLASK_ENV=development
 
+# method to convet ObjectId to string 
 def serial(dct):
     for k in dct:
         if isinstance(dct[k], ObjectId):
@@ -98,6 +100,9 @@ def create_book():
                         except expression as e:
                             return jsonify({"success": False, "error": e})
                     return jsonify({"success": True})
+                else:
+                    return jsonify({"success": False, "message": "book not exist"}), 401
+            # delete a book
             if data["action"] == "delete":
                 if "isbn" in data:
                     try:
@@ -106,9 +111,9 @@ def create_book():
                         return jsonify({"success": False, "error": e})
                 return jsonify({"success": True})
             else:
-                return jsonify({"success": False, "action": "action error"}), 301
+                return jsonify({"success": False, "action": "action error"}), 401
         else:
-            return jsonify({"success": False, "action": "not exist"}), 301
+            return jsonify({"success": False, "action": "not exist"}), 401
     else:
         return jsonify({"error": "request failed"}), 401
 
@@ -121,7 +126,7 @@ def users():
         data = request.get_json()
         if "action" in data:
             if data["action"] == "get_users":
-                orgs = [serial(item) for item in users.find({}, user)]
+                orgs = [serial(item) for item in users.find()]
                 return jsonify({"success": True, "users": orgs}), 201
             else:
                 return jsonify({"success": False, "action": "action error"}), 400
@@ -132,17 +137,21 @@ def users():
 
 @app.route('/register', methods=['POST'])
 def register():
-    """  """
-    users.db.users
+    """ create, update and delete a user """
+    users = db.users
     user = {}
     if request.method == 'POST':
         data = request.get_json()
         if 'action' in data:
+            # Create user
             if data['action'] == 'create':
+                org = users.find_one({'username': data.get("username")})
+                if org :
+                    return jsonify({"success": False, "message": "this user exist"}), 401    
                 if data.get("password") != "":
-                    pw_hash = bcrypt.generate_password_hash(data.get("password"))
+                    pw_hash = generate_password_hash(data.get("password"))
                 else:
-                    pw_hash=""
+                    pw_hash = ""
                 user['username'] = data.get("username", "")
                 user['password'] = pw_hash
                 user['type_user'] = data.get("type_user", "")
@@ -163,6 +172,60 @@ def register():
                 except expression as e:
                     return jsonify({"success": False, "error": e}), 400
                 return jsonify({"success": True}), 201
+            # Update user
+            if data["action"] == "update":
+                if "_id" in data:
+                    org = books.find_one({'_id': data.get("_id")})
+                    if org :
+                        if data.get("password") != "":
+                            pw_hash = generate_password_hash(data.get("password"))
+                        else:
+                            pw_hash = ""
+                        user['password'] = pw_hash
+                        if data.get('username'):
+                            user['username'] = data.get("username")
+                        if data.get('type_user'):
+                            user['type_user'] = data.get("type_user")
+                        if data.get('last_name'):
+                            user['last_name'] = data.get("last_name", "")
+                        if data.get('first_name'):    
+                            user['first_name'] = data.get("first_name", "")
+                        if data.get('date_born'):
+                            user['date_born'] = data.get("date_born", "")
+                        if data.get('scolar_year'):
+                            user['scolar_year'] = data.get("scolar_year", "")
+                        if data.get('classe'):
+                            user['classe'] = data.get("classe", "")
+                        if data.get('nbr_emprunt'):
+                            user['nbr_emprunt'] = data.get("nbr_emprunt", 0)
+                        if data.get('mail'):
+                            user['mail'] = data.get("mail", "")
+                        if data.get('phone'):
+                            user['phone'] = data.get("phone", "")
+                        if data.get('book_yet'):
+                            user['book_yet'] = data.get("book_yet", [])
+                        if data.get('book_read'):
+                            user['book_read'] = data.get("book_read", [])
+                        if data.get('address'):
+                            user['address'] = data.get("address", {})
+                        try:
+                            _id = data.get("_id")
+                            users.update_one( { '_id': _id } ,{ "$set": user}) 
+                        except expression as e:
+                            return jsonify({"success": False, "error": e})
+                    return jsonify({"success": True})
+                else:
+                    return jsonify({"success": False, "message": "user not exist"}), 401
+            # Delete 
+            if data["action"] == "delete":
+                if "_id" in data:
+                    try:
+                        users.delete_one({"_id": ObjectId(data.get("_id"))})
+                    except expression as e:
+                        return jsonify({"success": False, "error": e})
+                return jsonify({"success": True})
+            else:
+                return jsonify({"success": False, "action": "action error"}), 401
         else:
             return jsonify({"success": False, "action": "action error"}), 400
     else:
